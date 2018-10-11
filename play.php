0b034eb0818f61dbd0380d588b34d52734f75eec
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-10-11 11:14:25 +0200

    Class methods chapter

--- play.php
+++ play.php
@@ -3,9 +3,30 @@
 class Ship
 {
     public $name;
+
+    public $weaponPower = 0;
+
+    public $jediFactor = 0;
+
+    public $strength = 0;
+
+    public function sayHello()
+    {
+        echo 'Hello!';
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
 }
 
 $myShip = new Ship();
 $myShip->name = 'TIE Fighter';
+$myShip->weaponPower = 10;
 
-echo 'Ship name: '.$myShip->name;
+echo 'Ship name: '.$myShip->getName();
+echo '<hr>';
+$myShip->sayHello();
+echo '<hr>';
+var_dump($myShip->weaponPower);
