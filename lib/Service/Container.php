8c763e2242f309baa667656c5e62df481a897fe9
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-10-15 17:19:18 +0200

    AbstractShipStorage chapter

--- lib/Service/Container.php
+++ lib/Service/Container.php
@@ -51,10 +51,14 @@ class Container
         return $this->battleManager;
     }
 
+    /**
+     * @return \AbstractShipStorage
+     */
     public function getShipStorage()
     {
         if ($this->shipStorage === null) {
-            $this->shipStorage = new PdoShipStorage($this->getPDO());
+//            $this->shipStorage = new PdoShipStorage($this->getPDO());
+            $this->shipStorage = new JsonFileShipStorage(__DIR__.'/../../resources/ships.json');
         }
 
         return $this->shipStorage;
